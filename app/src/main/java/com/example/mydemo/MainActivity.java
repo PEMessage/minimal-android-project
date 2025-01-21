59bc8e4faf1c222b4df9b0b4e040f872c4ed9c76
PEMessage <1165739182@qq.com>
2025-01-21 20:51:54 +0800

    add toast hello world

--- app/src/main/java/com/example/mydemo/MainActivity.java
+++ app/src/main/java/com/example/mydemo/MainActivity.java
@@ -2,6 +2,9 @@
 package com.example.mydemo;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,8 +13,27 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private void showMessage(String msg) {
+        Toast.makeText(this,"[" + TAG + "]: " + msg, Toast.LENGTH_SHORT).show();
+    }
+
+    private void postOnCreated() {
+        // 1. bind id to java  object
+        Button button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showMessage("Hello world");return;
+            }
+        });
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Auto gen boilerplate
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
@@ -20,5 +42,8 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Actually code
+        postOnCreated();
     }
 }
